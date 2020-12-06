0f69c6073501efd237d85484f7c735f6226057da
hatoya0703 <hatoya0703@gmail.com>
2020-12-06 18:29:42 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
+  validates :first_name, presence: true,
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :last_name, presence: true,
+            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+  validates :kana_first_name, presence: true,
+            format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :kana_last_name, presence: true,
+            format: { with: /\A[ァ-ヶ]+\z/ }
+  validates :birth_date, presence: true
 end
