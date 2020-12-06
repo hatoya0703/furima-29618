753cc6c2b51c026b2ac4937303b34afc68a4bcf0
hatoya0703 <hatoya0703@gmail.com>
2020-12-06 18:27:45 +0900

    deviseストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    add_columns = [:nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birth_date]
+    devise_parameter_sanitizer.permit(:sign_up, keys:  add_columns)
+  end
 end
