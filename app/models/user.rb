276557c0d10ba4a0062b6848c0691897da7b19bd
hatoya0703 <hatoya0703@gmail.com>
2020-12-08 19:23:33 +0900

    コードレビュー後修正１
    モデルファイルのバリデーション表記をwith_optionsを使って省略。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true,
-            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 6 },
-            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
-  validates :first_name, presence: true,
-            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :last_name, presence: true,
-            format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :kana_first_name, presence: true,
-            format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :kana_last_name, presence: true,
-            format: { with: /\A[ァ-ヶ]+\z/ }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true,
+              format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, length: { minimum: 6 },
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶ]+\z/ } do
+      validates :kana_first_name
+      validates :kana_last_name
+    end
+    validates :birth_date
+  end
 end
