4fb52725b0183271f989c7791e17b82c80ec53e5
hatoya0703 <hatoya0703@gmail.com>
2020-12-19 17:47:45 +0900

    ActiveHash項目修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     category_id            { Faker::Number.between(from: 2, to: 10) }
     price                  { Faker::Number.between(from: 333, to: 9_999_999) }
     status_id              { Faker::Number.between(from: 2, to: 6) }
-    delivery_fee_bearer_id { Faker::Number.between(from: 2, to: 7) }
+    delivery_fee_bearer_id { Faker::Number.between(from: 2, to: 3) }
     sender_area_id         { Faker::Number.between(from: 2, to: 10) }
     shipping_date_id       { Faker::Number.between(from: 2, to: 6) }
     association :user
