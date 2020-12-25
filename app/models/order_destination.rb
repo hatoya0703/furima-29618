354e88e0b70e49f90426674d0b356a6f7bd2bd18
hatoya0703 <hatoya0703@gmail.com>
2020-12-25 21:46:40 +0900

    formオブジェクト作成
    ordersテーブルとdestinationsテーブルへまとめて値を保存するため

---
+++ app/models/order_destination.rb
@@ -0,0 +1,21 @@
+class OrderDestination
+
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building, :phone, :token
+
+# バリデーション
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone, format: { with: /\A[0-9]{,11}\z/}
+    validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Destination.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building: building, phone: phone, order_id: order.id)
+  end
+
+end
