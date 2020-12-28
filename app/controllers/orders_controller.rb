d8933462acab8ac9d1669d2ab5750a1d1a750862
hatoya0703 <hatoya0703@gmail.com>
2020-12-28 17:04:03 +0900

    リファクタリング
    購入済か否かの条件分岐の記述をシンプルに

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
 
   def index
     # 商品が既に購入されている時は、トップページに遷移させる
-    if Order.find_by(item_id:@item.id).nil?
+    if @item.order.nil?
       # 出品者がindexアクションに遷移しようとした場合、トップページに遷移させる
       if current_user.id != @item.user_id
         @order_destination = OrderDestination.new
@@ -21,7 +21,7 @@ class OrdersController < ApplicationController
   def create
     @order_destination = OrderDestination.new(order_params)
     # 商品が既に購入されている時は、トップページに遷移させる
-    if Order.find_by(item_id:@item.id).nil?
+    if @item.order.nil?
       # Payjpにトークンを送信
       if @order_destination.valid?
         payjp_charge
@@ -38,8 +38,9 @@ class OrdersController < ApplicationController
   end
 
   private
+
   def order_params
-    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(token: params[:token], user_id: current_user.id,item_id: params[:item_id])
+    params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :house_number, :building, :phone).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
   def set_item
@@ -47,7 +48,7 @@ class OrdersController < ApplicationController
   end
 
   def payjp_charge
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
