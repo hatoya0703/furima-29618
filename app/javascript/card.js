432ebab58a2ad97fa7c76f0ba9e46661b05037ac
hatoya0703 <hatoya0703@gmail.com>
2020-12-29 17:13:10 +0900

    デバッグコード削除

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -12,7 +12,6 @@ const pay = () => {
       exp_year: `20${formData.get("order_destination[exp_year]")}`,
       cvc: formData.get("order_destination[cvc]")
     };
-console.log(card);
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
