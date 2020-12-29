318ec06d8fa1f36dc055f443555b14000e4c1d83
hatoya0703 <hatoya0703@gmail.com>
2020-12-29 21:20:12 +0900

    basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
     add_columns = [:nickname, :first_name, :last_name, :kana_first_name, :kana_last_name, :birth_date]
     devise_parameter_sanitizer.permit(:sign_up, keys:  add_columns)
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'hatoya0703' && password == '1989hayato'
+    end
   end
 end
